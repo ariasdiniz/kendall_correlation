22b29c6dd5c5fcd5618f7256edd226c59f784792
Aria Diniz <aria.diniz.dev@gmail.com>
2024-06-17 10:33:44 -0300

    fixing typo

--- kcorr.h
+++ kcorr.h
@@ -1,14 +1,14 @@
-#ifndef __KENDELL_CORRELATION
-#define __KENDELL_CORRELATION
+#ifndef __KENDALL_CORRELATION
+#define __KENDALL_CORRELATION
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <csvparser.h>
 
 /*
-  Calculates the Kendell Correlation between the two first columns
+  Calculates the Kendall Correlation between the two first columns
   of the given CSV file.
 */
 double kcorr(char *csv_file_path, char *col_sep, unsigned int csv_header);
 
-#endif // __KENDELL_CORRELATION
+#endif // __KENDALL_CORRELATION
